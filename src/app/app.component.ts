17cbeb9e54fb09ed2f61788c815ea1374ae852c2
Simon Frezard <simon.frezard@gmail.com>
2021-02-04 14:36:52 +0100

    created sidemenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() { }
+
+  appPages = [
+    {
+      title: 'The News',
+      url: '/posts',
+      icon: 'newspaper'
+    }
+  ]
 }
