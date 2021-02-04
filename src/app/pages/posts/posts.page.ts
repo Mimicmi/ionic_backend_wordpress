e935399e3fedb8f8f4abad8bdf3f59656b30f456
Simon Frezard <simon.frezard@gmail.com>
2021-02-04 12:01:35 +0100

    added infinite scroll

--- src/app/pages/posts/posts.page.ts
+++ src/app/pages/posts/posts.page.ts
@@ -13,24 +13,50 @@ export class PostsPage implements OnInit {
   page = 1;
   posts = [];
   totalPosts = 0;
+  totalPages = 0;
 
   constructor(private api: ApiService,
     public loadingCtrl: LoadingController
   ) { }
 
 
-  async loadPosts() {
-    const loading = await this.loadingCtrl.create({
-      message: "loading posts"
-    });
-    await loading.present();
+  async loadPosts(infiniteScroll = null) {
+
+    let loading = null;
+
+    if (!infiniteScroll) {
+      loading = await this.loadingCtrl.create({
+        message: "loading posts"
+      });
+      await loading.present();
+    }
 
     this.api.getPosts(this.page).subscribe((res) => {
+
+      if (infiniteScroll) {
+        infiniteScroll.target.complete();
+        this.posts = [...this.posts, ...res.posts];
+
+        if (this.page == this.totalPages) {
+          infiniteScroll.target.disabled = true;
+        }
+      } else {
+        this.posts = res.posts;
+      }
+
+      this.totalPages = res.pages;
       this.totalPosts = res.totalPosts;
-      this.posts = res.posts;
       console.log('loadPosts: ', res);
-      loading.dismiss();
+
+      if (!infiniteScroll) {
+        loading.dismiss();
+      }
     });
+  }
+
+  loadMore(event) {
+    this.page++;
+    this.loadPosts(event);
   }
 
   ngOnInit() {
